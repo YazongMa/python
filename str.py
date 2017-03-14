4f5f0f1e4aefd6e86dde19c2f69f1ce1fd8c4e4d
YazongMa <mayazong@126.com>
2017-03-14 12:54:34 +0800

    first commit

---
+++ str.py
@@ -0,0 +1,12 @@
+
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+ 
+str = 'Hello World!'
+test = 'python' 
+print 'str         ' + str           
+print 'str[0]      ' + str[0]        
+print 'str[2:5]    ' + str[2:5]      
+print 'str[2:]     ' + str[2:]       
+print 'str*2       ' + str * 2       
+print 'str' + test + str + "TEST"  
