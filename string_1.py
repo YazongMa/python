cfcadaab8abeca2e5ed9baa159d5643808f53b3f
YazongMa <mayazong@126.com>
2017-03-24 09:04:36 +0800

    modify string_1.py

--- string_1.py
+++ string_1.py
@@ -9,5 +9,6 @@ print 'str         ' + str
 print 'str[0]      ' + str[0]        
 print 'str[2:5]    ' + str[2:5]      
 print 'str[2:]     ' + str[2:]       
+print 'str[2:3]    ' + str[2:3]       
 print 'str*2       ' + str * 2       
 print 'str' + test + str + "TEST"  
