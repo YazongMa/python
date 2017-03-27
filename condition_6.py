b482bb539d136d8a7ac0f5f7c444fd131c1bbc4d
YazongMa <mayazong@126.com>
2017-03-27 17:05:19 +0800

    modify condition.py

--- condition_6.py
+++ condition_6.py
@@ -35,9 +35,12 @@ for letter in 'Python':     # 第一个实例
  
 fruits = ['banana', 'apple',  'mango']
 for fruit in fruits:        # 第二个实例
-   print '当前水果 :', fruit
+    print '当前水果 :', fruit
 
-print range(2)
+print 'range(3)', range(3)
+rng_list = [0, 1, 2, 3]
+for index in range(len(rng_list)) :
+    print rng_list[index]
 
 for index in range(len(fruits)):
-	print fruits[index]
+    print fruits[index]
